8a75ee805845ee68c108fec019945130b74bccc8
waliuddin1105 <waliahmed1105@gmail.com>
2026-01-13 18:59:05 +0500

    Added celery config for background tasks and automation

---
+++ student management system/celery_config.py
@@ -0,0 +1,20 @@
+from celery import Celery
+from celery.schedules import crontab
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        broker=app.config['CELERY_BROKER_URL'],
+        backend=app.config['CELERY_RESULT_BACKEND']
+    )
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+        
+    celery.Task = ContextTask
+    return celery
+
